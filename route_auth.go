923da4fdf7bc0077a02f8f51ca010de577d5e593
Keegan Obonyo <keeganobonyo@gmail.com>
2018-06-29 13:01:09 +0300

    bug fix for the chat-app api for better error handling and serving of data

--- route_auth.go
+++ route_auth.go
@@ -8,78 +8,102 @@ import (
 	"net/http"
 )
 
-// GET /login
-// Show the login page
+var errormessage string = "POST"
+var successmessage string = "User created successfully"
 
 // POST /signup
-// Create the user account
-type NewUser struct {
-	name     string
+//Create a new user
+func signupAccount(writer http.ResponseWriter, request *http.Request) {
+	user := data.User{}
+	body, err := ioutil.ReadAll(io.LimitReader(request.Body, 1048576))
+	if err != nil {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode("Data limit exceeded")
+	}
+	if err := request.Body.Close(); err != nil {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode("Invalid request body")
+	}
+	if err := json.Unmarshal(body, &user); err != nil {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(writer).Encode(errormessage); err != nil {
+			writer.Header().Set("Content-Type", "application/json")
+			writer.WriteHeader(400)
+			json.NewEncoder(writer).Encode("Invalid json data")
+		}
+	}
+	if err := user.Create(); err != nil {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode("Couldn't create user")
+	} else {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusCreated)
+		json.NewEncoder(writer).Encode(successmessage)
+	}
+}
+
+// POST /authenticate
+// Authenticate the user given the email and password
+
+type Login struct {
 	email    string
 	password string
 }
 
-func (f NewUser) Name() string {
-	return f.name
-}
-func (f NewUser) Email() string {
+func (f Login) Email() string {
 	return f.email
 }
-func (f NewUser) Password() string {
+func (f Login) Password() string {
 	return f.password
-}
-func signupAccount(writer http.ResponseWriter, request *http.Request) {
-	newuser := NewUser{}
-	body, err := ioutil.ReadAll(io.LimitReader(request.Body, 1048576))
-	if err != nil {
-		danger(err, "No data posted")
-	}
-	if err := request.Body.Close(); err != nil {
-		danger(err, "errors")
-	}
-	if err := json.Unmarshal(body, &newuser); err != nil {
-		writer.Header().Set("Content-Type", "application/json")
-		writer.WriteHeader(422) // unprocessable entity
-		if err := json.NewEncoder(writer).Encode(err); err != nil {
-			danger(err, "errors")
-		}
-	}
-	user := data.User{
-		Name:     newuser.Name(),
-		Email:    newuser.Email(),
-		Password: newuser.Password(),
-	}
-	t := user.Create()
-	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(writer).Encode(t); err != nil {
-		danger(err, "Couldn't create User")
-	}
-	http.Redirect(writer, request, "/authenticate", 302)
 }
 
 // POST /authenticate
 // Authenticate the user given the email and password
 func authenticate(writer http.ResponseWriter, request *http.Request) {
-	err := request.ParseForm()
-	user, err := data.UserByEmail(request.PostFormValue("email"))
+	login := Login{}
+	body, err := ioutil.ReadAll(io.LimitReader(request.Body, 1048576))
 	if err != nil {
-		danger(err, "Cannot find user")
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode("Data limit exceeded")
 	}
-	if user.Password == data.Encrypt(request.PostFormValue("password")) {
+	if err := request.Body.Close(); err != nil {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode("Invalid request body")
+	}
+	if err := json.Unmarshal(body, &login); err != nil {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(writer).Encode(errormessage); err != nil {
+			danger(err, "errors")
+		}
+	}
+	// err := request.ParseForm()
+	user, err := data.UserByEmail(login.Email())
+	if err != nil {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode("Couldn't find user")
+	}
+	if user.Password == data.Encrypt(login.Email()) {
 		session, err := user.CreateSession()
 		if err != nil {
-			danger(err, "Cannot create session")
+			writer.Header().Set("Content-Type", "application/json")
+			writer.WriteHeader(400)
+			json.NewEncoder(writer).Encode("Error creating session")
+		} else {
+			cookie := http.Cookie{
+				Name:     "_cookie",
+				Value:    session.Uuid,
+				HttpOnly: true,
+			}
+			http.SetCookie(writer, &cookie)
 		}
-		cookie := http.Cookie{
-			Name:     "_cookie",
-			Value:    session.Uuid,
-			HttpOnly: true,
-		}
-		http.SetCookie(writer, &cookie)
-		http.Redirect(writer, request, "/", 302)
-	} else {
-		http.Redirect(writer, request, "/signup", 302)
 	}
 
 }
@@ -89,9 +113,10 @@ func authenticate(writer http.ResponseWriter, request *http.Request) {
 func logout(writer http.ResponseWriter, request *http.Request) {
 	cookie, err := request.Cookie("_cookie")
 	if err != http.ErrNoCookie {
-		warning(err, "Failed to get cookie")
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode("Failed to get cookie")
 		session := data.Session{Uuid: cookie.Value}
 		session.DeleteByUUID()
 	}
-	http.Redirect(writer, request, "/", 302)
 }
--- route_thread.go
+++ route_thread.go
@@ -3,7 +3,6 @@ package main
 import (
 	"./data"
 	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -27,35 +26,33 @@ func createThread(writer http.ResponseWriter, request *http.Request) {
 	sess, err := session(writer, request)
 	thread := Thread{}
 	if err != nil {
-		http.Redirect(writer, request, "/authenticate", 302)
+		error_message(writer, request, "Invalid session")
 	} else {
 
 		body, err := ioutil.ReadAll(io.LimitReader(request.Body, 1048576))
 		if err != nil {
-			http.Redirect(writer, request, "/authenticate", 302)
+			error_message(writer, request, "Data limit exceeded")
 		}
 		if err := request.Body.Close(); err != nil {
-			http.Redirect(writer, request, "/authenticate", 302)
+			error_message(writer, request, "Invalid data")
 		}
 		if err := json.Unmarshal(body, &thread); err != nil {
 			writer.Header().Set("Content-Type", "application/json")
 			writer.WriteHeader(422)
-			if err := json.NewEncoder(writer).Encode(err); err != nil {
-				http.Redirect(writer, request, "/authenticate", 302)
+			json.NewEncoder(writer).Encode("Invalid json data")
+		} else {
+			topic := thread.Name()
+			user, err := sess.User()
+			if err != nil {
+				danger(err, "Cannot get user from session")
+			}
+			writer.Header().Set("Content-Type", "application/json")
+			writer.WriteHeader(http.StatusCreated)
+			if _, err := user.CreateThread(topic); err != nil {
+				danger(err, "Cannot create thread")
 			}
 		}
-		topic := thread.Name()
-		user, err := sess.User()
-		if err != nil {
-			danger(err, "Cannot get user from session")
-		}
-		writer.Header().Set("Content-Type", "application/json")
-		writer.WriteHeader(http.StatusCreated)
-		if _, err := user.CreateThread(topic); err != nil {
-			danger(err, "Cannot create thread")
-		}
 	}
-	http.Redirect(writer, request, "/", 302)
 }
 
 // GET /thread/read
@@ -64,15 +61,14 @@ func readThread(writer http.ResponseWriter, request *http.Request) {
 	uuid := vals.Get("id")
 	thread, err := data.ThreadByUUID(uuid)
 	if err != nil {
-		error_message(writer, request, "Cannot read thread")
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode("Cannot read thread")
 	} else {
 		_, err := session(writer, request)
 		if err != nil {
-			// generateHTML(writer, &thread, "layout", "public.navbar", "public.thread")
-			{
-				writer.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(writer).Encode(thread)
-			}
+			writer.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(writer).Encode("Invalid session")
 		} else {
 			writer.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(writer).Encode(thread)
@@ -99,39 +95,36 @@ func postThread(writer http.ResponseWriter, request *http.Request) {
 	sess, err := session(writer, request)
 	postthread := PostThread{}
 	if err != nil {
-		http.Redirect(writer, request, "/authenticate", 302)
+		error_message(writer, request, "Invalid session")
 	} else {
 
 		body, err := ioutil.ReadAll(io.LimitReader(request.Body, 104857655))
 		if err != nil {
-			http.Redirect(writer, request, "/authenticate", 302)
+			error_message(writer, request, "Data limit excceeded")
 		}
 		if err := request.Body.Close(); err != nil {
-			http.Redirect(writer, request, "/authenticate", 302)
+			error_message(writer, request, "Invalid request body")
 		}
 		if err := json.Unmarshal(body, &postthread); err != nil {
 			writer.Header().Set("Content-Type", "application/json")
 			writer.WriteHeader(422)
-			if err := json.NewEncoder(writer).Encode(err); err != nil {
-				http.Redirect(writer, request, "/authenticate", 302)
+			json.NewEncoder(writer).Encode("Invalid Json")
+		} else {
+			user, err := sess.User()
+			if err != nil {
+				danger(err, "Cannot get user from session")
+			}
+
+			postbody := postthread.Body()
+			uuid := postthread.Uuid()
+
+			thread, err := data.ThreadByUUID(uuid)
+			if err != nil {
+				error_message(writer, request, "No previous thread to embedd")
+			}
+			if _, err := user.CreatePost(thread, postbody); err != nil {
+				danger(err, "Cannot create post")
 			}
 		}
-		user, err := sess.User()
-		if err != nil {
-			danger(err, "Cannot get user from session")
-		}
-
-		postbody := postthread.Body()
-		uuid := postthread.Uuid()
-
-		thread, err := data.ThreadByUUID(uuid)
-		if err != nil {
-			error_message(writer, request, "Cannot read thread")
-		}
-		if _, err := user.CreatePost(thread, postbody); err != nil {
-			danger(err, "Cannot create post")
-		}
-		url := fmt.Sprint("/thread/read?id=", uuid)
-		http.Redirect(writer, request, url, 302)
 	}
 }
